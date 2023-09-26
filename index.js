bff86868f0fd52202de814dcb17842adf131a260
Ana Ruiz <aruiz_90@hotmail.com>
2023-09-26 16:46:18 -0500

    block13

--- index.js
+++ index.js
@@ -5,8 +5,11 @@
  * @param {string} fahren temperature in degrees F
  * @returns {number} the number of degrees C
  */
-function convertToCelsius(fahren) {}
+function convertToCelsius(fahren) {
+  return (fahren - 32) * (.56);
+}
 
+console.log(convertToCelsius(95));
 /**
  * Takes both numbers (F, C) and display a message with
  * both numbers and how that temp makes you feel using
@@ -22,37 +25,54 @@ function convertToCelsius(fahren) {}
  * @param {number} fahren
  * @param {number} celc
  */
-function createMessage(fahren, celc) {}
+ function createMessage(fahren, celc) {
+   let message = ""
+    if(fahren < 32 && celc < 0) {
+       message = `${fahren}, ${celc}, very cold`;
+     }
+       else if(fahren < 64 && celc < 18) {
+         message = `${fahren}, ${celc}, cold`;
+       }
+         else if(fahren < 86 && cecl < 30) {
+           message = `${fahren}, ${celc}, warm`;
+         } else if(fahren <100 && celc < 37) {
+          message = `${fahren}, ${celc}, hot`;
+         }
+         return message;
+ }
+ console.log(createMessage(95,35));
 
 /**
  * Takes a number and returns a random integer from 0 to the limit
  * @param {number} limit
  * @returns {number} a number between 0 and the int passed in
  */
-function rand(limit) {}
-
+function rand(limit) {
+  return Math.floor(Math.random() * limit);
+}
+console.log(rand(10));
 // -------------------- DONT NOT CHANGE THE CODE BELOW ---------------------- //
 
-let fahren = prompt(
-  "enter a number, we will convert that number from fahrenheit to celcius"
-);
-let celc = convertToCelsius(fahren);
-let output = createMessage(fahren, celc);
-console.log(output);
+// let fahren = prompt(
+//   "enter a number, we will convert that number from fahrenheit to celcius"
+// );
+// let celc = convertToCelsius(fahren);
+// let output = createMessage(fahren, celc);
+// console.log(output);
 
-fahren = prompt(
-  "Lets try that again. enter a number, we will convert that number from fahrenheit to celcius"
-);
-celc = convertToCelsius(fahren);
-output = createMessage(fahren, celc);
-console.log(output);
+// fahren = prompt(
+//   "Lets try that again. enter a number, we will convert that number from fahrenheit to celcius"
+// );
+// celc = convertToCelsius(fahren);
+// output = createMessage(fahren, celc);
+// console.log(output);
 
-fahren = rand(110);
-celc = convertToCelsius(fahren);
-output = createMessage(fahren, celc);
-console.log(output);
+// fahren = rand(110);
+// celc = convertToCelsius(fahren);
+// output = createMessage(fahren, celc);
+// console.log(output);
 
-fahren = rand(110);
-celc = convertToCelsius(fahren);
-output = createMessage(fahren, celc);
-console.log(output);
+// fahren = rand(110);
+// celc = convertToCelsius(fahren);
+// output = createMessage(fahren, celc);
+// console.log(output);
